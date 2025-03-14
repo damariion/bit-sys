42b50f94b69a8943e57530a186951decb22ca1f5
Damarion Abendanon <41810775+damariion@users.noreply.github.com>
2025-03-14 15:19:17 +0100

    Create installer.ts

---
+++ installer.ts
@@ -0,0 +1,74 @@
+export class Installer
+{
+    // stored locally for
+    // simplicity and modifiability
+    #sources =
+    {
+        "sys":[
+            "seq.json",
+            "ssm.ts",
+        ],
+
+        "lib":[
+            "nsapi.ts"
+        ],
+
+        "srv":[
+            "lshost.ts",
+            "lsprog.ts",
+            "nsx.ts",
+            "rnuke.ts",
+            "rsync.ts",
+        ],
+
+        "rmt":[
+            "rnsapi.ts",
+        ],
+
+        "bin":[
+            "instruct.ts", 
+            "rpurge.ts"
+            ],
+    }
+
+    #ns;
+    constructor (ns: NS)
+    {
+        this.#ns = ns;
+    }
+
+    *#urls (): Generator<[string, string] | null>
+    {
+        let dstr = "bitsys/";
+        let srcr = "https://raw.githubusercontent.com/" +
+                   "damariion/bit-sys/refs/heads/main/";
+
+        for (let dir in this.#sources)
+        {
+            for (let file of this.#sources[dir])
+            {
+                yield [
+                    (srcr + dir + '/' + file), 
+                    (dstr + dir + '/' + file)
+                    ]
+            }
+        }
+
+        yield null;
+    }
+
+    start()
+    {
+        let urls = this.#urls();
+        let data: IteratorResult<[string, string] | null>;
+
+        while ((data = urls.next().value) != null)
+            this.#ns.wget(data[0], data[1], "home");
+    }
+}
+
+export async function main(ns: NS) 
+{
+    let installer = new Installer(ns);
+    installer.start();
+}
