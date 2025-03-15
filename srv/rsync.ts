1364e2120446408d190ec4af17aaba4fa30835cb
Damarion Abendanon <41810775+damariion@users.noreply.github.com>
2025-03-15 12:24:26 +0100

    Update rsync.ts

--- srv/rsync.ts
+++ srv/rsync.ts
@@ -1,26 +1,30 @@
 import {NSAPI} from 'bitsys/lib/nsapi.ts';
+import {SysAPI} from 'bitsys/lib/sysapi.ts';
 
 export async function main(ns: NS) 
 {   
     // initialization
-    let nsapi = new NSAPI(ns);
-    let root  = "bitsys/rmt/";    
+    let nsapi  = new NSAPI(ns);
+    let sysapi = new SysAPI(nsapi);
+    let root   = "bitsys/rmt/";    
+
+    // efficient termination
+    if (!await sysapi.directoryExists(root))
+        return;
 
     let hosts = JSON.parse(ns.read("bitsys/var/hosts.json"));
     let files = Array<string>(await nsapi.call("ls", "home", root));
-    
-    // disallow empty directories
-    if (files.length === 0) return;
 
     // copy all contents of /rmt
     for (let host in hosts)
     {
+        // clean system
+        for (let file of await nsapi.call("ls", host))
+            await nsapi.call("rm", file, host);
+
         for (let path of files)
         {
             let file = String(path).replace(root, '');
-
-            // remove old file
-            await nsapi.call("rm", file, host);
 
             // implemented for clean navigation on remote
             await nsapi.call("mv", "home", root + file, file);
