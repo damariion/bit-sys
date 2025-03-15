828c355822576c27deeeb8d14c39ba657d84364a
Damarion Abendanon <41810775+damariion@users.noreply.github.com>
2025-03-15 12:21:10 +0100

    Create sysapi.ts

---
+++ lib/sysapi.ts
@@ -0,0 +1,32 @@
+import {NSAPI} from 'bitsys/lib/nsapi.ts';
+
+export class SysAPI
+{
+    #nsapi;
+    constructor (nsapi: NSAPI)
+    {
+        this.#nsapi = nsapi;
+    }
+
+    async directoryExists(path: string)
+    {
+        // absence of items = nonexistent
+        return (Array(await this.#nsapi.call(
+            "ls", "host", path)).length > 0);
+    }
+
+    async sizeOf(path: string)
+    {
+        // singular file
+        if (!await this.directoryExists(path))
+            return String(await this.#nsapi.call("read", path)).length;
+
+        // directory size
+        let size = 0;
+        for (let file of await this.#nsapi.call("ls", "home", path))
+            size += String(await this.#nsapi.call("read", file)).length;
+
+        return size;
+    }
+    
+}
